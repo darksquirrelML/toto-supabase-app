2cb06c605edd06304585d771183287b3976ea062
darksquirrelML <107252023+darksquirrelML@users.noreply.github.com>
2026-01-26 16:30:20 +0800

    Add files via upload

--- toto_app_supabase.py
+++ toto_app_supabase.py
@@ -23,6 +23,8 @@ from supabase import create_client
 SUPABASE_URL = "https://fcibqtbavrltcvzhfgjy.supabase.co"
 SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjaWJxdGJhdnJsdGN2emhmZ2p5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0MDIwNjMsImV4cCI6MjA4NDk3ODA2M30.dZCE7TpUZWHnT3vUvuviAZqfi9_MFwqkQHBk0RZNb9A"
 
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 
 ###############################################################
 # ---------------------------
