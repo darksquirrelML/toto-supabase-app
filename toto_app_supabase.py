7a1c6d2f7725c99142f6d80302f50705fd57af8b
darksquirrelML <107252023+darksquirrelML@users.noreply.github.com>
2026-01-26 16:41:04 +0800

    Add files via upload

--- toto_app_supabase.py
+++ toto_app_supabase.py
@@ -20,8 +20,19 @@ import time
 from supabase import create_client
 
 
-SUPABASE_URL = "https://fcibqtbavrltcvzhfgjy.supabase.co"
-SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjaWJxdGJhdnJsdGN2emhmZ2p5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0MDIwNjMsImV4cCI6MjA4NDk3ODA2M30.dZCE7TpUZWHnT3vUvuviAZqfi9_MFwqkQHBk0RZNb9A"
+# SUPABASE_URL = "https://fcibqtbavrltcvzhfgjy.supabase.co"
+# SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjaWJxdGJhdnJsdGN2emhmZ2p5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0MDIwNjMsImV4cCI6MjA4NDk3ODA2M30.dZCE7TpUZWHnT3vUvuviAZqfi9_MFwqkQHBk0RZNb9A"
+
+
+# Load from Streamlit Secrets
+SUPABASE_URL = st.secrets["https://fcibqtbavrltcvzhfgjy.supabase.co"]
+SUPABASE_KEY = st.secrets["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjaWJxdGJhdnJsdGN2emhmZ2p5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0MDIwNjMsImV4cCI6MjA4NDk3ODA2M30.dZCE7TpUZWHnT3vUvuviAZqfi9_MFwqkQHBk0RZNb9A"
+"]
+
+# Create Supabase client
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
